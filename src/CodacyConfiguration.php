cff36bfea1b864a66f830efd304fa9d3c4d6fdb7
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-12-11 13:07:33 +0100

    clean: Use generator directly in filesFromConfiguration

--- src/CodacyConfiguration.php
+++ src/CodacyConfiguration.php
@@ -11,31 +11,28 @@ function stringEndsWith($str, $test)
 
 function filesFromConfiguration()
 {
-    $generator = function () {
-        $srcdir = '/src';
-        $codacyrcName = "/.codacyrc";
-        if (file_exists($codacyrcName)) {
-            $codacyrcFile = file_get_contents($codacyrcName);
-            $codacyrc = json_decode($codacyrcFile);
-            if (json_last_error() === JSON_ERROR_NONE && property_exists($codacyrc, 'files')) {
-                $files = $codacyrc->{'files'};
-                foreach ($files as $file) {
-                    yield join('/', [$srcdir, $file]);
-                }
-                return;
+    $srcdir = '/src';
+    $codacyrcName = "/.codacyrc";
+    if (file_exists($codacyrcName)) {
+        $codacyrcFile = file_get_contents($codacyrcName);
+        $codacyrc = json_decode($codacyrcFile);
+        if (json_last_error() === JSON_ERROR_NONE && property_exists($codacyrc, 'files')) {
+            $files = $codacyrc->{'files'};
+            foreach ($files as $file) {
+                yield join('/', [$srcdir, $file]);
             }
+            return;
         }
-        $rii = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($srcdir));
-        foreach ($rii as $file) {
-            if ($file->isDir()) {
-                continue;
-            }
-            $filename = $file->getPathname();
-            if (stringEndsWith($filename, ".php"))
-                yield $file->getPathname();
+    }
+    $rii = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($srcdir));
+    foreach ($rii as $file) {
+        if ($file->isDir()) {
+            continue;
         }
-    };
-    return iterator_to_array($generator());
+        $filename = $file->getPathname();
+        if (stringEndsWith($filename, ".php"))
+            yield $file->getPathname();
+    }
 }
 
 function addFilesToEngine(Engine $engine, $files)
